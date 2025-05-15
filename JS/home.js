e31fd7135dae091d1294930425368e9c6b4398da
neverevis <milan.b.dev@gmail.com>
2025-05-15 10:13:42 -0300

    atualizado função de adicionar livros na lista

--- JS/home.js
+++ JS/home.js
@@ -26,13 +26,16 @@ for(conta of Contas)
 //definir botões
 btn_registrar = document.getElementById('btn_registrar');
 btn_fechar_registro = document.getElementById('btn_fechar_registro')
+btn_enviar_registro = document.getElementById('btn_enviar_registro')
 
 //definir elementos
 modal_registro = document.getElementById('modal_registro');
+catalogo = document.getElementById('catalogo');
 
 //definir eventos
 btn_registrar.addEventListener('click',toggle_modal_registro);
 btn_fechar_registro.addEventListener('click',toggle_modal_registro);
+btn_enviar_registro.addEventListener('click',adicionar);
 
 //funcões
 function toggle_modal_registro(){
@@ -47,38 +50,63 @@ function toggle_modal_registro(){
 //adicionar filme registrado
 function adicionar()
 {
+    titulo = document.getElementById('titulo');
+    titulo = document.getElementById('ano');
+    titulo = document.getElementById('autor');
+    titulo = document.getElementById('categoria');
+    titulo = document.getElementById('capa');
+    titulo = document.getElementById('edicao');
+
     if(validar() === true)
     {
         let Livro =
         {
-            id: null,
             titulo: titulo.value,
             ano: ano.value,
             autor: autor.value,
             categoria: categoria.value,
-            prioridade: prioridade,value,
+            prioridade: 0,
             capa: capa.value,
             edicao: edicao.value,
             review: []
         }
 
         Livros.push(Livro);
-        Livros[Livros.length-1].id = Livros.length-1;
+        console.log(Livro)
         localStorage.setItem("Livros", JSON.stringify(Livros));
-        fecharModalReg();
+        toggle_modal_registro();
+        atualizar_catalogo(Livros);
     }
-
-    atualizarCatalogo(Livros);
 }
 
 function validar(){
     valido = true;
 
-    if(titulo.value == '' && titulo.value.length <= 255)
+    if(titulo.value === '' && titulo.value.length <= 255)
         valido = false;
-    if(ano.value > new Date.getFullYear)
+    if(ano.value >= new Date(Date.now()).getFullYear())
         valido = false;
-    if(autor.value == '')
+    if(autor.value === '')
         valido = false;
 
+    return valido;
 }
+
+function atualizar_catalogo(lista){
+    if(lista.length > 0){
+
+    }else{
+        mensagem = document.createElement('p');
+        mensagem.classList.add('text-muted')
+        mensagem.textContent = "Não há livros a serem exibidos aqui";
+        
+        mensagem1 = document.createElement('p');
+        mensagem1.classList.add('text-muted');
+        mensagem1.textContent = "Não há livros a serem exibidos aqui";
+
+        catalogo.appendChild(mensagem);
+        catalogo.appendChild(mensagem1);
+    }
+}
+
+atualizar_catalogo(Livros)
